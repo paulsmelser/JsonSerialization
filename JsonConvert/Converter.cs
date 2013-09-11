2473c8d692cbd4979dc06c12e490c6b00e86bbda
Paul Smelser <paul.smelser@gmail.com>
2013-09-10 23:38:59 -0400

    initial commit

---
+++ JsonConvert/Converter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace JsonConverterSample
+{
+    public class Converter
+    {
+        public static Interaction Deserialize(string stringRepresentation)
+        {
+            return JsonConvert.DeserializeObject<Interaction>(stringRepresentation,
+                new JsonSerializerSettings {TypeNameHandling = TypeNameHandling.Objects});
+        }
+
+        public static string Serialize(Interaction interaction)
+        {
+            return JsonConvert.SerializeObject(interaction,
+                new JsonSerializerSettings {TypeNameHandling = TypeNameHandling.Auto});
+        }
+    }
+
+    public class Interaction
+    {
+        public Guid Id { get; set; }
+        //[JsonProperty(TypeNameHandling = TypeNameHandling.Objects)]
+        public ITrigger Trigger { get; set; }
+        //[JsonProperty(TypeNameHandling = TypeNameHandling.Objects)]
+        public IObject Object { get; set; }
+        public Person Person { get; set; }
+    }
+
+    public interface ITrigger
+    {
+        string Name { get; set; }
+    }
+
+    public interface IObject
+    {
+        string Hello { get; set; }
+    }
+
+    public class Buy : ITrigger
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string Bye { get; set; }
+    }
+
+    public class Product : IObject
+    {
+        public string Type { get; set; } 
+        public string Hello { get; set; }
+        public double Price { get; set; }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ JsonConvert/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace JsonConverterSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var interaction = new Interaction
+            {
+                Id = new Guid(),
+                Object = new Product
+                {
+                    Type = "Product",
+                    Hello = "Hi",
+                    Price = 99.99
+                },
+                Person = new Person
+                {
+                    FirstName = "Paul",
+                    LastName = "Smelser"
+                },
+                Trigger = new Buy
+                {
+                    Type = "Buy",
+                    Bye = "Goodbye",
+                    Name = "TheName"
+                }
+            };
+
+//            var serialized = Converter.Serialize(interaction);
+//            var deserialized = Converter.Deserialize(serialized);
+//
+//            var price = ((Product) deserialized.Object).Price;
+
+            var serializedWithContract = JsonConvert.SerializeObject(interaction);
+           // var deserializeWithContract = JsonConvert.DeserializeObject<Interaction>(serializedWithContract);
+
+           // var priceContract = ((Product)deserializeWithContract.Object).Price;
+
+            var deserializedConverter = InteractionJsonConvert.DeseriealizeInteraction(serializedWithContract);
+        }
+    }
+}
